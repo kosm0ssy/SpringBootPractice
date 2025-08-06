a71e6ffde7ff6726b777cdb34fc215fda62df369
kosm0ssy <xzerokim@gmail.com>
2025-08-06 15:58:17 +0900

    first commit for feature/user branch

--- src/main/java/com/example/securitytest/controller/UserController.java
+++ src/main/java/com/example/securitytest/controller/UserController.java
@@ -1,11 +1,8 @@
 package com.example.securitytest.controller;
 
-import com.example.securitytest.dto.UserDto;
 import com.example.securitytest.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,9 +12,9 @@ public class UserController {
 
   private final UserService userService;
 
-  @RequestMapping(value = "", method = RequestMethod.POST)
-  public UserDto createUser(@RequestBody UserDto userDto) {
-    
-  }
+//  @RequestMapping(value = "", method = RequestMethod.POST)
+//  public UserDto createUser(@RequestBody UserDto userDto) {
+//
+//  }
 
 }
